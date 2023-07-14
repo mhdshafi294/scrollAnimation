d3495d03242acb1b51205896b229f6fd316de767
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-07-14 21:29:20 +0300

    done commit

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ window.addEventListener("scroll", setScrollVar);
 window.addEventListener("resize", setScrollVar);
 
 function setScrollVar() {
-  const htmlElement = document.documentElement
+  const htmlElement = document.documentElement;
   const percentOfScreenHeightScrolled =
-    htmlElement.scrollTop / htmlElement.clientHeight
-  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+    htmlElement.scrollTop / htmlElement.clientHeight;
+  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100));
   htmlElement.style.setProperty(
     "--scroll",
     Math.min(percentOfScreenHeightScrolled * 100, 100)
-  )
+  );
 }
 
 setScrollVar();
